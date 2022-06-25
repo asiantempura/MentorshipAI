0248bfecbf89f5d044aa201284a834e261127874
AnishPhadnis <anishphadnis.ca@gmail.com>
2022-06-24 23:35:49 -0400

    refactored modified functions for deap to be replaced by operator lib functions

---
+++ Anish/Week 2/modifiedFunctions.py
@@ -0,0 +1,45 @@
+def strip(s):
+    return s.strip()
+
+def lower(s):
+    return s.lower()
+
+def title(s):
+    return s.title()
+
+def upper(s):
+    return s.upper()
+
+def lstrip(s):
+    return s.lstrip()
+
+def rstrip(s):
+    return s.rstrip()
+
+def swapcase(s):
+    return s.swapcase()
+
+def testCallable(s):
+    return s()
+
+
+functions = {
+    'strip': strip,
+    'lower': lower,
+    'title': title, 
+    'upper': upper,
+    'lstrip': lstrip,
+    'rstrip': rstrip,
+    'swapcase': swapcase, 
+    'testCallable': testCallable
+}
+'''
+print(functions.get('strip')(' ABC '))
+print(functions.get('lower')(' ABC '))
+print(functions.get('title')(' ABC '))
+print(functions.get('upper')(' ABC '))
+print(functions.get('lstrip')(' ABC '))
+print(functions.get('rstrip')(' ABC '))
+print(functions.get('swapcase')(' ABC '))
+'''
+
