8512a37fb6620f19fe08bf0e59f43f90d36f58e2
AnishPhadnis <anishphadnis.ca@gmail.com>
2022-06-25 21:07:31 -0400

    partial eval func examples for deap primitives

--- Anish/Week 2/test.py
+++ Anish/Week 2/test.py
@@ -1,3 +1,50 @@
+from StringIO import StringIO 
+import sys
+from functools import partial
+
+def progn(*args):
+    for arg in args:
+        arg()
+
+def prog2(out1, out2): 
+    return partial(progn,out1,out2)
+
+def do1():
+    print(1)
+
+def do2():
+    print(2)
+
+def do3():
+    print(3)
+
+prog2(do1, do2)(prog2(do3, do2))
+
+
+
+def printSomething():
+    print("Hello there")
+    print("How you doin")
+    print("Not bad")
+
+
+class Capturing(list):
+    def __enter__(self):
+        self._stdout = sys.stdout
+        sys.stdout = self._stringio = StringIO()
+        return self
+    def __exit__(self, *args):
+        self.extend(self._stringio.getvalue().splitlines())
+        self._stringio.close() 
+        sys.stdout = self._stdout
+
+with Capturing() as output:
+    printSomething()
+
+'''print(output)'''
+
+# -----------
+
 import operator
 
 class Person:
@@ -6,6 +53,7 @@ class Person:
     def __init__(self):
         return
 
-p = Person()
+'''p = Person()
 name = operator.attrgetter("name")
-print(operator.attrgetter("name")(Person()))
+print(operator.attrgetter("name")(Person()))'''
+
